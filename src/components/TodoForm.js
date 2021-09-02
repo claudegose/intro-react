b4a9812f71777c68e1f5acab89e26d4cb9a6bed7
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-02 23:18:58 +0200

    Todo Form Completed and Imported/Displayed to App.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,3 +1,35 @@
 import React, {useState} from "react";
 
+function TodoForm () {
+    const [input, setInput] = useState("");
 
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // e.onSubmit({
+        //     id: Math.floor(Math.random() *10000),
+        //     text: input
+        // })
+
+        setInput("");
+
+    }
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Add a todo"
+                value={input}
+                name="text"
+                className="todo-input"
+                />
+
+            <button className="todo-button">Add Todo</button>
+        </form>
+
+
+    );
+}
+
+export default TodoForm;
