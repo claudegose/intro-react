efda997587708b479887d1c00502991603ff087a
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-03 00:05:28 +0200

    Install & Add React Icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import TodoForm from "./TodoForm";
+import {RiCloseCircleLine} from "react-icons/ri";
+import {TiEdit} from "react-icons/ti";
 
 
 function Todo () {
@@ -10,12 +12,23 @@ function Todo () {
 
 
 
-    return (
-        <div>
+    return todos.map((todo, index) => (
+        <div
+            className={"todo.isComplete ? "todo-row complete" :
+        "todo-row"} key={index}>
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            {todo.text}
+        </div>
+            <div className="icons">
+                <RiCloseCircleLine />
+                <TiEdit />
 
+
+
+            </div>
 
         </div>
-    )
+    ))
 }
 
 
