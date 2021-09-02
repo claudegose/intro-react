c8b3798a35f38c5add69fe4b5c337ccc115fd068
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-02 23:49:47 +0200

    Todo edit Function Progress Push

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,1 +1,22 @@
-TodoForm.js
+import React, {useState} from "react";
+import TodoForm from "./TodoForm";
+
+
+function Todo () {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ""
+    })
+
+
+
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+
+export default Todo;
