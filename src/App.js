88e1ffa1ebe1c6ff4ecc8c241bcd1d8d2ef2dd94
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-03 10:40:17 +0200

    Alterations Performed with prisitine detail and accuracy! AM-PM Cards Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import TodoList from "./components/TodoList";
+import TodoList2 from "./components/Todolist2";
 
 function App() {
   return (
     <div className="todo-app">
       <TodoList />
-
+        <TodoList2 />
     </div>
   );
 }
